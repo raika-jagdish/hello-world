8d3e7bfb364dc5f53b6f8903c57f96d6b29f103f
jagdishr <jagdish.raika@amdocs.com>
2020-04-04 03:10:08 +0530

    Initial commit

---
+++ scs-function-demo/src/main/java/com/jd/scs/ScsFunctionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jd.scs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsFunctionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScsFunctionDemoApplication.class, args);
+	}
+
+}
